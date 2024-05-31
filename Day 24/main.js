4a6ab3fffde327eda82a7317e3eb8c7b83318cd1
mickeysrishti <Mukeshpathak345@gmail.com>
2024-05-31 20:54:12 +0530

    Day 24 With GSAP

---
+++ Day 24/main.js
@@ -0,0 +1,36 @@
+gsap.from(".logo", {
+    duration: 1.5,
+    y: "-200",
+})
+gsap.from(".menu", {
+    delay: .5,
+    duration: 1.5,
+    y: "-200",
+})
+
+gsap.from(".right", {
+    delay: .7,
+    duration: 1.5,
+    x: "300",
+})
+
+gsap.from(".hero-btn", {
+    delay: .9,
+    duration: 1.5,
+    x: "-1300",
+})
+gsap.from("#page h1", {
+    delay: 1,
+    duration: 1.5,
+    y: "1300",
+})
+gsap.from("#page p", {
+    delay: 1.1,
+    duration: 1.5,
+    y: "1300",
+})
+gsap.from(".hero-last-btn", {
+    delay: 1.4,
+    duration: 1.5,
+    x: "1300",
+})
