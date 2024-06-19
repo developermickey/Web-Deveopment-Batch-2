3f2d759108c9ef0f4a165740600142f318512df8
mickeysrishti <Mukeshpathak345@gmail.com>
2024-06-19 21:20:44 +0530

    day 25

---
+++ Day 25/main.js
@@ -0,0 +1,43 @@
+gsap.from(".logo", {
+    y: "-120%",
+    duration: 1,
+
+})
+gsap.from(".menu", {
+    y: "-150%",
+    duration: 2
+
+})
+gsap.from(".btn-nav", {
+    y: "-150%",
+    duration: 1,
+    delay: 0.3
+
+})
+
+gsap.from("#home h2", {
+    x: "-150%",
+    duration: 1,
+    delay: 0.3
+
+})
+gsap.from("#home p", {
+    x: "-150%",
+    duration: 1,
+    delay: 0.3
+
+})
+
+gsap.from("#home .btn-group", {
+    x: "-150%",
+    duration: 1,
+    delay: 0.3
+
+})
+
+gsap.from("#home .rating-group", {
+    x: "-150%",
+    duration: 1,
+    delay: 0.3
+
+})
