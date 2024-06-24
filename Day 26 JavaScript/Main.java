bd998428ea8fe8a61654ac1734eebe81e5dbb57f
mickeysrishti <Mukeshpathak345@gmail.com>
2024-06-24 21:03:14 +0530

    Code JavaScript

---
+++ Day 26 JavaScript/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+public static void main(String[] args) {
+
+    int number = 50;
+    float numbers = 40.333f;
+    double numberss = 40.333777777777777777;
+    boolean isPass = true;
+
+    String stuName = "Samiksha"; 
+    String stuNames = 'a';
+
+
+}
+
+}
---
+++ Day 26 JavaScript/main.js
@@ -0,0 +1,16 @@
+// Student ka Data 
+
+var std_id = 1;
+var std_name = "mayur";
+var std_age = 22;
+var std_grade = "A";
+var std_Pass = false;
+
+
+
+console.log("Student ID : " +  std_id);
+console.log("Student Name : " +  std_name);
+console.log("Student Age : " +  std_age);
+console.log("Student Grade : " + std_grade);
+console.log("Student Pass/Fail : " +  std_Pass);
+
