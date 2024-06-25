61e6a9cc26fec5984c11c66bcc0837dee730b63f
mickeysrishti <Mukeshpathak345@gmail.com>
2024-06-25 21:11:33 +0530

    day 27

---
+++ Day 27 JavaScript/main.js
@@ -0,0 +1,29 @@
+// variables kya hote 
+
+// kisi bhi data ka name yeah value yeh aesa conatiner uske ander koi value store; 
+
+// conatiner ke under data store karna yeh usko name dena kya variable 
+
+// variable likhte kaise hai 
+
+var conatiner = 20;
+var conatiner_Name = "Class JavaScript";
+var conatiner_IsArived  = true;
+
+// datatypes 
+
+// String = "Mueksh ",  'A', "jkhfjksdhfjds" ;
+// Number =  10, 10.90, 10.00000000000000101010101010010101; -10;
+// Boolean = true/false, 
+
+console.log(conatiner + conatiner_Name + conatiner_IsArived)
+
+
+
+
+
+
+
+// console.log(conatiner); // print / access the conatiner variable
+// console.log(conatiner_Name); // print / access the conatiner variable
+// console.log(conatiner_IsArived); // print / access the conatiner variable
