08533106c19438d2e2115836436a345f1c778b34
mickeysrishti <Mukeshpathak345@gmail.com>
2024-06-26 20:59:43 +0530

    java SCript adde

---
+++ Day 28 JavaScript/main.js
@@ -0,0 +1,64 @@
+// variables hote hai data store karne ke liye or variable me data change bhi kar sakte hai 
+
+// var student_Name = "Mayur";
+// var student_Name = "Mukesh";  // Value updation 
+
+
+
+let std_Name = "Mayur";
+
+
+std_Name = "Mukesh"; // Value updation 
+let st_Age = 12;
+st_Age = 12220;
+
+// Constants 
+
+// pi = 3.14 constants value 
+
+const student_Parents_Name = "Mukesh ";
+
+let A = 20; 
+let B = 30;
+
+let C = A + B; 
+let D = A - B; 
+let E = A * B; 
+let F = A / B; 
+
+
+console.log(C);
+console.log(D);
+console.log(E);
+console.log(F);
+
+
+// Data Types 
+
+String
+Number 
+Boolean
+undefined // ek value 
+
+ 
+
+let a; // initialization 
+a = 20 ; // declaration
+console.log(a); //print // access 
+
+
+let b = null; 
+console.log(b);
+
+// window 
+alert(a);
+prompt("Enter Your Name");
+
+
+// how to store a variable after given promts 
+
+
+
+
+
+
