2351e4fc7377fb0fb223f4a0bdff70327143283d
mickeysrishti <Mukeshpathak345@gmail.com>
2024-06-28 21:10:53 +0530

    Day 29

---
+++ Day 29 JavaScript/main.js
@@ -0,0 +1,56 @@
+// variables & constants ✅  var, let and const 
+// compilers & interpreters ✅
+// window ✅ 
+// types
+// undefined not defined null
+// Operatars 
+// conditionals
+// loops
+// functions
+// return
+// arrays
+// objects
+// questions
+
+
+
+
+// compilers & interpreters  
+// computer 0 or 1  
+
+
+
+
+//  Windows  
+
+
+// var a = Number(prompt("Enter A"));
+// var b = Number(prompt("Enter B"));
+
+// let name = "Mayur";
+// alert("hello " + name);
+
+
+// confirm("Are you under 18 or not ?");  
+
+// prompt = jab bhi user se input lena ho hum prompt user 
+
+// take a input from user print Full Name ?
+
+// var fullname = prompt("Enter You Full Name");
+
+// console.log("User Full Name: " + fullname);  
+// var a = 2000000;
+// console.log("Tesitng ke Liye or user bug finder ke liye use karte hai ")
+
+// console.error("your password is incorrect");
+// console.warn("If you are adults then go ahead");
+// let obj = {
+//     name: "Mukesh Pathak",
+//     age: 28,
+//     address: "Bhopal"
+
+// }
+// console.table(obj);
+
+
