372121348106e41c2a139da137a9aece0da197ee
mickeysrishti <Mukeshpathak345@gmail.com>
2024-07-16 21:07:35 +0530

    Day 33 Added

---
+++ Day 33 JavaScript/main.js
@@ -0,0 +1,518 @@
+// compilers & interpreters ✅
+// variables & constants ✅  var, let and const 
+// window ✅ 
+// undefined not defined null ✅ 
+// types ✅ 
+// Operatars - ✅
+// conditionals
+// loops
+
+// Advaced Topic 
+// functions 
+// loops
+// return
+// arrays   
+// objects
+
+
+
+// variables & constants  
+
+
+
+
+// var a = 20; // yah ek variable hai  var hai wo ek keyword 
+// var a = 10; 
+
+// var num1 = 10;
+// var num1 = 40;
+// var num1 = 100;
+// var num2 = 30;
+
+// var sumofTwoNumber = num1 + num2; // 
+
+// console.log(sumofTwoNumber);
+
+// constants
+
+// const user = "Samiksha";
+
+// user = "Sam";
+// console.log(user);
+
+
+// variables craete kar sakte ho 
+
+// Var ----- Done Jiski value change kar sakte hai 
+// let 
+// Const ---Done Jiski value change nahi kar sakte 
+
+// var firstName = "Mukesh"; // Identifier 
+// var firstName = "Mayur";  // Identifier  updated value
+// var firstName = "Sam";  // Identifier  updated value
+
+
+// let firstName = "Mueksh"; // yaha par declar kiya hai 
+
+// firstName = "Sam"; // Yaha par variable ko update kiya hai 
+
+
+
+// console.log(firstName);
+
+
+
+// window
+// alert()
+// prompt()
+// console.log()
+// confirm()
+// document 
+
+// alert("Are you Adults?");
+
+
+// Kisi bhi user se input lene ke liye hum prompt ko use karte hai 
+
+// let fullname = prompt("Enter Your Full Name ?");
+
+
+// console.log(fullname);  // variable access / Print 
+
+
+// user se only option choose karne ke liye eska use hota hai  like cookies 
+// confirm("Are you under 18");
+
+
+
+// console print karne ke liye use hota hai 
+
+// console.log("Server Running in Port 5500");
+
+// console.error("Enter password is wrong!!!")
+
+// console.warn("userData is overloading!!!")
+
+// let obj = {
+//     name: "Mukesh Pathak",
+//     age: 28,
+//     mobile: "8359921640"
+// }
+
+
+// console.table(obj);
+
+
+
+// questions
+
+
+
+
+
+// let a = 20;
+
+// console.log(a);
+
+
+
+// compilers & interpreters  
+// computer 0 or 1  
+
+
+
+
+//  Windows  
+
+
+// var a = Number(prompt("Enter A"));
+// var b = Number(prompt("Enter B"));
+
+// let name = "Mayur";
+// alert("hello " + name);
+
+
+// confirm("Are you under 18 or not ?");  
+
+// prompt = jab bhi user se input lena ho hum prompt user 
+
+// take a input from user print Full Name ?
+
+// var fullname = prompt("Enter You Full Name");
+
+// console.log("User Full Name: " + fullname);  
+// var a = 2000000;
+// console.log("Tesitng ke Liye or user bug finder ke liye use karte hai ")
+
+// console.error("your password is incorrect");
+// console.warn("If you are adults then go ahead");
+// let obj = {
+//     name: "Mukesh Pathak",
+//     age: 28,
+//     address: "Bhopal"
+
+// }
+// console.table(obj);
+
+
+
+
+
+
+
+
+
+
+
+// function sum(a, b){
+   
+//     return a + b;
+
+// }
+
+// console.log(sum(10, 20))
+
+
+
+
+// Day 31 Content=============================== //
+
+// undefined not defined null
+
+// let a;  // declaration 
+// a = 10;  // initialization
+
+// console.log(a); //
+
+
+// null // null ek value hoti jiska hindi me meaning hota hia kuch nahi;
+
+// let fullname = null;
+
+// console.log(fullname);
+
+// Data types // Data ka roop 
+
+// 2 type ke data types hote hai 
+// Primitive Data Types
+
+// String  // text "Mukesh Addres 573489573485"
+// Number // 20 , 1000.0000, 1.1
+// Boolean //true, false
+// Undefined
+// Null
+// Bigint
+// Symbol
+
+
+// Reference Data Types
+
+// [] {} ()
+
+// primitive data type studing
+// String ? // Jo value apko text ke format me chahiye wo ap String me likhte ho 
+
+// let usernane = "@mauyr1234556"; // "" '' sara text 
+
+// console.log(typeof usernane);
+
+
+// how to create a variables mean rules kya hai /
+
+// ap koi letter start $, _, 
+
+// let fullname = "Mukesh Pathak"; // vaild Variable
+// let $fullname = "Mukesh Pathak"; // vaild Variable
+// let _fullname = "Mukesh Pathak"; // vaild Variable
+// let full_name = "Mukesh Pathak"; // vaild Variable
+// let fullname_ = "Mukesh Pathak"; // vaild Variable
+// let fullname1233 = "Mukesh Pathak"; // vaild Variable
+// let fullName = "Mukesh Pathak"; // vaild Variable
+
+// let 12133fullname = "Mukesh Pathak"; // Non-vaild Variable
+// let full-name = "Mukesh Pathak"; // Non-vaild Variable
+// let full name = "Mukesh Pathak"; // Non-vaild Variable
+// let @full name = "Mukesh Pathak"; // Non-vaild Variable
+
+
+
+// Primitive Data Types 
+
+// 1. String (All text include )
+
+// let userName = '@technicalmickey';
+// let userName1 = 't';
+
+// console.log(userName);
+// console.log(userName1);
+
+
+// 2. Number (All Number include )
+
+// let num = 10;
+// let floatingValue = 10.10; // float value
+
+// console.log(num);
+// console.log(floatingValue);
+
+
+// 3. Boolean (All Boolean include (2 Value true / false) )
+
+// let isStudentPass = false; 
+// console.log(isStudentPass);
+
+
+
+// 4. undefined kuch nahi hai 
+
+// let ball = undefined;
+
+// console.log(ball);
+
+
+// 5. Null data types 
+
+// let userName = null;
+// ===================================
+
+// Reference 
+
+// [] array of objects 
+
+// {}  object 
+ 
+// () methods
+
+
+// let studentMarks = [12, "Mukesh", "1233445", true, 12000, false];
+// console.log(studentMarks);
+
+
+// Varibales //   var let const 
+
+// var fullName = "mukesh";
+// var fullName1 = "rakesh";
+// var fullName2 = "duaa";
+// var fullName3 = "shyam";
+// var fullName4 = "ravi";
+
+// var a = 20;
+// var b = 30;
+
+// console.log("This is the value of a & b= " + (a + b));
+
+// var n = 10;
+// var n = 20;
+
+// let n = 10; 
+// n = 20; 
+
+// console.log(n);
+
+
+
+// Constants //
+// jinki value fix hoti hai
+// exa PI = 3.14;
+
+
+// const pi = 3.14;
+// pi = 3;
+
+
+
+
+
+
+// DataTypes //
+
+// 2 Types of DataTypes in JS 
+// 1. Primitive Types
+// Number 
+// String 
+// Boolean 
+// null 
+// undefined
+// BigInit
+// Symbol 
+
+// typeof variable ka name ;
+
+
+// Reference Types 
+// [] Arrays ;
+// {} Oobject
+// () Methods 
+
+// console.log();
+// console.error();
+// console.warn();
+
+
+
+
+// New Topic Date 16 July 2024
+
+// Operatars - 
+
+
+// 1. Assignment Operatar " = "
+
+// let a = 20;
+
+
+// 2. Addition Operatar " + "
+
+// let b = 40;
+
+// let sum = a + b;
+
+// console.log(sum);
+
+
+
+// 3. Multiplication Operatar " * "
+
+// let x = 5;
+// let y = 10;
+
+// let z = x * y;
+
+// console.log(z);
+
+
+
+// 4. compression Operatar " > "
+
+
+// let x = 5;
+// let y = 10;
+
+// let compare = x < y;
+
+
+// console.log(compare);
+
+
+// Type of JavaScript Operators 
+
+// Arithmetic Operators 
+// Assignments Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+
+
+
+// Arithmetic Operators  
+
+// + Addtion
+// - Subtraction
+// * Multiplication
+// / Division
+// % Modulus
+// ++ Increment 
+// -- Decrement
+
+// + Addtion
+// let addition = 20 + 30; // 50
+
+// - Subtraction
+
+// let subtraction = 20 - 30;  // -10
+
+// * Multiplication
+
+// let multiplication = 2 * 3; // 6
+
+
+
+//  Division
+
+// let division = 10 / 5; // 2
+
+// console.log(division);
+
+
+
+// % Modulus
+
+// let remainder = 25 % 2;
+
+// console.log(remainder);
+
+
+
+// ++ Increment 
+
+let a = 10;
+
+// Post Increment
+// console.log(a++); // Post increment Bad Me update hogi value  10
+// console.log(a); // a = a + 1;
+
+
+
+// Pre Increment
+
+// console.log(++a); // Pre increment Pahle Update hogi value 11
+
+
+
+// -- Decrement
+// Post Decrement
+// console.log(a--); // Post Decrement  Bad me value update hogi
+// console.log(a)
+
+
+// Pre Decrement Pahle Value Update Hogi 
+// console.log(--a)
+
+
+// let x = 10;
+// let y = 10;
+
+// Yaha par x and y jo hia operand and jo +  Sign hia wo hai operator 
+// let z  = x + y;
+
+
+//  2 Power 2 == 4
+
+// ** Exponentiation Yah Power Lagane ke kam me aata hai 
+
+// let x = 5;
+
+// let z  = x ** 2;
+
+// (z);
+
+// console.log(Math.pow(5, 2));
+
+
+
+// Assignment Operators 
+
+// =, +=, -=, *=, /=, %= 
+
+// let x = 10;
+// x %= 5;   // x = x + 5;
+
+// console.log(x)
+
+
+// conditionals 
+
+let light = "OFF";
+
+if(light == "ON"){
+    console.log("Light ON Hai");
+}else{
+    // agar if ture nahi huaa to yah kar do 
+
+    console.log("Light OFF Hai");
+}
+
+
+
