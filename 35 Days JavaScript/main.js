fc78c10ac2ec56503ee2cc70bdc7a50f312a63e0
mickeysrishti <Mukeshpathak345@gmail.com>
2024-08-30 21:10:23 +0530

    If else nested if else added

---
+++ 35 Days JavaScript/main.js
@@ -0,0 +1,55 @@
+// If 
+// else if
+// and else 
+
+
+// let studentName = "John"; 
+
+// if(studentName == "Mukesh") {
+//     console.log("Student is Mukesh");
+// }else if(studentName == "John"){
+//     console.log("Now Student is John is else if 2 ");
+// }else if(studentName == "Sakshi"){
+//     console.log("Student is Sakshi");
+// }
+// else{
+//     console.log("Student is Rakesh");
+// }
+
+// Nested If else  
+
+// let std_Mask = 94;  
+
+
+
+
+
+// if(std_Mask > 50){ 
+//     //nested if else 
+//     if(std_Mask >= 95){
+//         console.log("Grade A++");
+//     }else if(std_Mask >= 91) {
+//         console.log("Grade A+");
+//     }else {
+//         console.log("Grade B");
+//     }
+// }else{
+//     console.log("Grade C");
+// }
+
+
+
+// function 
+// String => methoed
+// Loops // For, While , For in , for of 
+// Arrays: methoed   
+// Events:  Click, dbClick, mouseover , mouseout, mouseenter , mouseleave
+
+// DOM 
+
+
+x = 10; 
+
+console.log(x);
+
+var x;
