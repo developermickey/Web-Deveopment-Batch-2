17f0ce5561097b460ad584ba4d0df3fd42a61b0e
mickeysrishti <Mukeshpathak345@gmail.com>
2024-09-03 21:15:10 +0530

    Strings

---
+++ Strings/main.js
@@ -0,0 +1,52 @@
+// String 
+
+// String are for storing text 
+
+// "hello world" 
+
+// let fullName = "Mukesh"; //text mukesh 
+// let lastName = 'Pathak'; //text mukesh  
+// let age = 28;
+
+// template literal  String 
+
+// console.log(`i am ${fullName} ${lastName}`); // ` `
+
+
+// console.log(`My name is ${fullName} ${lastName} and My age is ${age}`)
+
+
+// length is property 
+
+// let words = "Lorem ipsum dolor sit amet, consectetur adip";
+
+// console.log(words.length);
+
+// let myself = 'it\'s me';
+// let myself = "Hello this is my friends \"John\" me";
+
+// let myself = "\\";
+
+// console.log(myself);
+
+
+// String Methods 
+
+// 1. method charAt();
+
+// let name = "Mukesh";
+
+// console.log(name.charAt(0));
+// console.log(name.charCodeAt(1));
+
+// console.log(name.at(0));
+
+// console.log(name[0]);
+
+
+// Stirng toUpperCase();
+
+
+let fullName = "mukesh pathak";
+
+console.log(fullName.toUpperCase());
