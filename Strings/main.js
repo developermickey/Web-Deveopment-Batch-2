799b39fdf4a7b683a6928bab3aab46400acb1a3e
mickeysrishti <Mukeshpathak345@gmail.com>
2024-09-09 21:20:20 +0530

    String method

--- Strings/main.js
+++ Strings/main.js
@@ -47,6 +47,82 @@
 // Stirng toUpperCase();
 
 
-let fullName = "mukesh pathak";
+// let fullName = "mukesh pathak";
 
-console.log(fullName.toUpperCase());
+// console.log(fullName.toUpperCase());
+
+
+
+// String Text String me hum text ko store kar sakte hai  ""
+
+// example 
+
+// let firstName = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil quas cumque soluta autem veniam, deleniti ea voluptas sunt fugit voluptate? Asperiores, consequatur facere voluptatum sit in id dolorem tempore?";
+// let lastName = 'Pathak';
+
+// console.log(typeof firstName, typeof lastName);
+
+// console.log(firstName.length);
+
+// what is index only every element postion
+
+
+// console.log(lastName[3]);
+
+
+
+// Slice ek method string  
+
+// syntex  
+
+// let fullName = "Mukesh Pathak";
+
+// slice 2 value ek start jo ki include hota hai 
+// or ek hota hai end  jo ki exclude hota hai = jise gina nahi jayega 
+
+// console.log(fullName.slice(7, 10)); 
+
+// console.log(fullName.substring(7, 10))
+
+// console.log(fullName.substr(1)) 
+// first value index and second value length 
+
+// let helloString = "THIS IS MY CLASS";
+
+// // console.log(helloString.toUpperCase());
+// console.log(helloString.toLowerCase());
+
+
+
+ 
+// id="demo"
+// id="btn"
+
+
+// let p = document.getElementById("demo");
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//     // console.log("demo clicked");
+
+//    p.innerHTML = toUppercase();
+
+
+// })
+
+
+let f1 = "Hello";
+let f2 = "World";
+
+// console.log(f1 + " " + f2);
+
+// console.log(f1.concat(" ", f2))
+
+// trim method             mukeshpathak345@gmail.com
+
+let username = "         mike@123  hdjfdkf    "
+console.log(username.length);
+
+console.log(username.trim())
+console.log(username.length);
+
